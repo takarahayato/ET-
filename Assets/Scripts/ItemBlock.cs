f4e684769e96404c38e9a6b8dd0fe18a836e5972
matayu <e185742@ie.u-ryukyu.ac.jp>
2020-08-11 17:31:14 +0900

    add hatena block

---
+++ Assets/Scripts/ItemBlock.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemBlock : MonoBehaviour
+{
+    public GroundCheck hatena;
+
+    private bool isHatena = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        isHatena = hatena.IsGround();
+
+        if (isHatena)
+        {
+            Debug.Log("block!!!!");
+        }
+        
+    }
+}
